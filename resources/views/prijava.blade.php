8d85b6aab993e2b8bc2d3970c9facc1549448d6c
janborisek <42959870+janborisek@users.noreply.github.com>
2018-12-18 08:37:00 +0100

    start

--- resources/views/prijava.blade.php
+++ resources/views/prijava.blade.php
@@ -1,2 +1,1 @@
 <div>neki za prjavt al</div>
-<iframe src="https://docs.google.com/forms/d/e/1FAIpQLScILR9PCvDsw1pZ_T8H4dtF_yqYyAz95OFpARfYZA1CfyyGBw/viewform?embedded=true" width="640" height="477" frameborder="0" marginheight="0" marginwidth="0">Nalaganje ...</iframe>
